596f4d2af313eb17cb8c2fb8d54c80819c607379
Juan Pablo Ortiz Rubio <ortizrubio09@gmail.com>
2022-11-11 21:06:15 -0500

    fix(backend): arreglo configs backend y db (#20)

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,14 +1,20 @@
 import os
 
-import motor.motor_asyncio
 from dotenv import load_dotenv
 from tinydb import TinyDB
+from tinydb.storages import JSONStorage
+from tinydb_serialization import SerializationMiddleware
+from tinydb_serialization.serializers import DateTimeSerializer
+
+from LSC_recognizer_model.src.coordenates.models.coordenates_models import (
+    INPUT_SHAPE_FIX,
+    get_model_coord_dense_3,
+)
 
 # ----------------- Environment Variables ----------------- #
 
 load_dotenv()
 PYTHONUNBUFFERED = os.getenv("PYTHONUNBUFFERED")
-DB_URL = os.getenv("DB_URL")
 SECRET = os.getenv("SECRET")
 
 # ----------------- PATHS --------------------------
@@ -16,21 +22,22 @@ SECRET = os.getenv("SECRET")
 PATH_PREDICTED_IMG = os.getenv("PATH_PREDICTED_IMG")
 
 PATH_RAW_SIGNS = os.getenv("PATH_RAW_SIGNS")
-PATH_RAW_NUMPY = os.getenv("PATH_RAW_NUMPY")
+PATH_NUMPY_COORDS = os.getenv("PATH_NUMPY_COORDS")
 
-PATH_CHECKPOINTS_LOAD = os.getenv("PATH_CHECKPOINTS_LOAD")
-PATH_CHECKPOINTS_SAVE = os.getenv("PATH_CHECKPOINTS_SAVE")
-
-# ----------------- Database variables (MongoDB) --------------------------
-
-client = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
-db = client.TesisDB
-signal_table = db.signals
-user_table = db.users
-
-# ----------------- Database variables (MongoDB) --------------------------
-
+PATH_MODELS = os.getenv("PATH_MODELS")
 PATH_DB = os.getenv("PATH_DB")
-tinydb = TinyDB(PATH_DB + "/db.json")
-signal_table_tinydb = tinydb.table("signals")
-user_table_tinydb = tinydb.table("users")
+
+# ----------------- Database variables --------------------------
+
+serialization = SerializationMiddleware(JSONStorage)
+serialization.register_serializer(DateTimeSerializer(), "TinyDate")
+
+tinydb = TinyDB(PATH_DB + "/db.json", storage=serialization)
+signal_table = tinydb.table("signals")
+users_table = tinydb.table("users")
+models_table = tinydb.table("models")
+
+# ----------------- Model variables --------------------------
+
+neuronal_network = get_model_coord_dense_3
+INPUT = INPUT_SHAPE_FIX
